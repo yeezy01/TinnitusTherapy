2fa1e59eba49e2033fa92c424ccf5ff0ded6d6af
Yemi Yusuf <yemi.yusuf93@gmail.com>
2024-11-15 01:44:58 +0000

    Update to load TinnitusTherapy

---
+++ service-worker.js
@@ -0,0 +1,25 @@
+const cacheName = 'tinnitus-therapy-cache-v1';
+const filesToCache = [
+  './tinnitus-therapy.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(cacheName)
+      .then(cache => {
+        return cache.addAll(filesToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
+});
